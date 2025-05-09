611e04d46141d11566addf919e79b3b0ca4ca02a
Marco Meyer-Conde　　　  メイヤーマコト <37673095+meiyasan@users.noreply.github.com>
2025-05-09 17:05:55 +0800

    Update build.sh

--- recipe/build.sh
+++ recipe/build.sh
@@ -3,7 +3,13 @@
 [ -f "$PREFIX/lib/libHepMC2.so" ] && ln -sf libHepMC2.so $PREFIX/lib/libHepMC.so
 [ -f "$PREFIX/lib/libHepMC3.so" ] && ln -sf libHepMC3.so $PREFIX/lib/libHepMC.so
 
-./configure --prefix=$PREFIX
+if [ -f "CMakeLists.txt" ]; then
+  mkdir build
+  cd build
+  cmake .. -DCMAKE_INSTALL_PREFIX=$PREFIX
+else
+  ./configure --prefix=$PREFIX --with-hepmc=$PREFIX
+fi
 
 make -j$(nproc)
 make install
