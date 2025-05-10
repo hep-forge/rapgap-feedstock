d922fd75b11977b74d92789af71caab49c48cb9b
Marco Meyer-Conde <marco@tcu.ac.jp>
2025-05-10 13:20:44 +0900

    update recipe to EIC repository

--- recipe/build.sh
+++ recipe/build.sh
@@ -3,13 +3,7 @@
 [ -f "$PREFIX/lib/libHepMC2.so" ] && ln -sf libHepMC2.so $PREFIX/lib/libHepMC.so
 [ -f "$PREFIX/lib/libHepMC3.so" ] && ln -sf libHepMC3.so $PREFIX/lib/libHepMC.so
 
-if [ -f "CMakeLists.txt" ]; then
-  mkdir build
-  cd build
-  cmake .. -DCMAKE_INSTALL_PREFIX=$PREFIX -DCMAKE_CXX_STANDARD=17
-else
-  ./configure --prefix=$PREFIX --with-hepmc=$PREFIX
-fi
+./configure --prefix=$PREFIX --with-hepmc=$PREFIX
 
 make -j$(nproc)
 make install
