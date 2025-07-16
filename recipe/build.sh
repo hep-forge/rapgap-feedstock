2cc5489e519dd758b8cf138e5f30eb7291568a14
Marco Meyer-Conde <marco@tcu.ac.jp>
2025-07-16 23:55:59 +0900

    update build

--- recipe/build.sh
+++ recipe/build.sh
@@ -10,12 +10,7 @@ export CXXFLAGS="-std=c++17 ${CXXFLAGS:-}"
 if [ -f "CMakeLists.txt" ]; then
   mkdir -p build
   cd build
-  cmake .. \
-    -DCMAKE_INSTALL_PREFIX="$PREFIX" \
-    -DCMAKE_CXX_STANDARD=17 \
-    -DCMAKE_CXX_STANDARD_REQUIRED=ON \
-    -DCMAKE_CXX_EXTENSIONS=OFF \
-    -DCMAKE_CXX_FLAGS="$CXXFLAGS"
+  cmake .. -DCMAKE_INSTALL_PREFIX="$PREFIX" -DCMAKE_CXX_STANDARD=17
 else
   ./configure --prefix="$PREFIX" --with-hepmc="$PREFIX" CXXFLAGS="$CXXFLAGS"
 fi
