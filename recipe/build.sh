2c13754d073be1e36097d3b9146b18a5d2a66023
Marco Meyer-Conde　　　  メイヤーマコト <37673095+meiyasan@users.noreply.github.com>
2025-08-19 17:27:32 +0900

    Update build.sh

--- recipe/build.sh
+++ recipe/build.sh
@@ -5,12 +5,12 @@
 [ -f "$PREFIX/lib/libHepMC3.so" ] && ln -sf libHepMC3.so "$PREFIX/lib/libHepMC.so"
 
 # Set CXX standard flags
-export CXXFLAGS="-std=c++17 ${CXXFLAGS:-}"
-
+export ROOT_CXX_STANDARD=$(root-config --cflags | sed -n 's/.*-std=c++\([0-9]*\).*/\1/p')
+export CXXFLAGS="-std=c++${ROOT_CXX_STANDARD} ${CXXFLAGS:-}"
 if [ -f "CMakeLists.txt" ]; then
   mkdir -p build
   cd build
-  cmake .. -DCMAKE_INSTALL_PREFIX="$PREFIX" -DCMAKE_CXX_STANDARD=17
+  cmake .. -DCMAKE_INSTALL_PREFIX="$PREFIX" -DCMAKE_CXX_STANDARD=${ROOT_CXX_STANDARD}
 else
   ./configure --prefix="$PREFIX" --with-hepmc="$PREFIX" CXXFLAGS="$CXXFLAGS"
 fi
